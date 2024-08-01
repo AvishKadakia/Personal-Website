505524f098be21d2b3f8db1e2eac8e2f50f95e01
Avish Kadakia <avishkadakia1996@gmail.com>
2024-08-01 16:40:37 -0400

    Refactored Page404 and Particle.js

--- src/Components/Page404/Page404.js
+++ src/Components/Page404/Page404.js
@@ -5,38 +5,35 @@ import splatterDark from "../../Images/splatterdark.png";
 
 import Particle from "../Particle/Particle";
 
-class Page404 extends React.Component {
-  componentDidMount() {}
-  render() {
-    return (
-      <React.Fragment>
-        <div className={"coming-soon-container " + this.props.className}>
-          <div className="background">
-            <Particle lineColor="#fff" lineOpacity="0.8"></Particle>
+const Page404 = ({ className, message }) => {
+  return (
+    <React.Fragment>
+      <div className={"coming-soon-container " + className}>
+        <div className="background">
+          <Particle lineColor="#fff" lineOpacity="0.8"></Particle>
 
-            <div className="line" id="line1"></div>
-            <div className="line" id="line2"></div>
-            <div className="line" id="line3"></div>
-            <div className="line" id="line4"></div>
-            <div className="line" id="line5"></div>
+          <div className="line" id="line1"></div>
+          <div className="line" id="line2"></div>
+          <div className="line" id="line3"></div>
+          <div className="line" id="line4"></div>
+          <div className="line" id="line5"></div>
+        </div>
+        <div className="circle-section">
+          <div className="splatter">
+            <img alt="Splatter Dark" src={splatterDark}></img>
           </div>
-          <div className="circle-section">
-            <div className="splatter">
-              <img alt="Splatter Dark" src={splatterDark}></img>
-            </div>
-            <div className="circle-big">
-              {/*Reference Website: https://codepen.io/FlorinPop17/pen/PooXqaQ */}
-              <div className="blob-animation circle-small"></div>
+          <div className="circle-big">
+            {/* Reference Website: https://codepen.io/FlorinPop17/pen/PooXqaQ */}
+            <div className="blob-animation circle-small"></div>
 
-              <div className="heading-container">
-                <h1 className="type-wrap typed">{this.props.message} !!!!!!!!!!</h1>
-              </div>
+            <div className="heading-container">
+              <h1 className="type-wrap typed">{message} !!!!!!!!!!</h1>
             </div>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Page404;
--- src/Components/Particle/Particle.js
+++ src/Components/Particle/Particle.js
@@ -1,135 +1,135 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Particle.scss";
 import Particles from "react-particles-js";
 
-class Particle extends React.Component {
-  componentDidMount() {}
-  render() {
-    let lineColor = this.props.lineColor;
+const Particle = ({ lineColor, lineOpacity, className }) => {
+  const [noOfParticles, setNoOfParticles] = useState(15);
 
-    let noOfParticles = 15;
-    let lineOpacity = this.props.lineOpacity ? this.props.lineOpacity : 0.2;
+  useEffect(() => {
     if (window.innerWidth < 800) {
-      noOfParticles = 22;
+      setNoOfParticles(22);
     }
-    return (
-      <React.Fragment>
-        <Particles
-          className={"particle-container  " + this.props.className}
-          params={{
-            particles: {
-              number: {
-                value: noOfParticles,
-                density: {
-                  enable: true,
-                  value_area: 315.65905665290904,
-                },
-              },
-              color: {
-                value: "#ffffff",
-              },
-              shape: {
-                type: "circle",
-                stroke: {
-                  width: 0,
-                  color: "#000000",
-                },
-                polygon: {
-                  nb_sides: 20,
-                },
-                image: {
-                  src: "img/github.svg",
-                  width: 100,
-                  height: 100,
-                },
-              },
-              opacity: {
-                value: 0.2244776885211732,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false,
-                },
-              },
-              size: {
-                value: 0,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 400,
-                  size_min: 0.1,
-                  sync: false,
-                },
-              },
-              line_linked: {
+  }, []);
+
+  const adjustedLineOpacity = lineOpacity ? lineOpacity : 0.2;
+
+  return (
+    <React.Fragment>
+      <Particles
+        className={"particle-container " + className}
+        params={{
+          particles: {
+            number: {
+              value: noOfParticles,
+              density: {
                 enable: true,
-                distance: 150,
-                color: lineColor,
-                opacity: lineOpacity,
-                width: 1,
-              },
-              move: {
-                enable: true,
-                speed: 2,
-                direction: "none",
-                random: false,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200,
-                },
+                value_area: 315.65905665290904,
               },
             },
-            interactivity: {
-              detect_on: "canvas",
-              events: {
-                onhover: {
-                  enable: false,
-                  mode: "repulse",
-                },
-                onclick: {
-                  enable: false,
-                  mode: "push",
-                },
-                resize: true,
+            color: {
+              value: "#ffffff",
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000",
               },
-              modes: {
-                grab: {
-                  distance: 400,
-                  line_linked: {
-                    opacity: 1,
-                  },
-                },
-                bubble: {
-                  distance: 400,
-                  size: 40,
-                  duration: 2,
-                  opacity: 8,
-                  speed: 3,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-                push: {
-                  particles_nb: 4,
-                },
-                remove: {
-                  particles_nb: 2,
-                },
+              polygon: {
+                nb_sides: 20,
+              },
+              image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100,
               },
             },
-            retina_detect: true,
-          }}
-        ></Particles>
-      </React.Fragment>
-    );
-  }
-}
+            opacity: {
+              value: 0.2244776885211732,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 0,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 400,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: lineColor,
+              opacity: adjustedLineOpacity,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: {
+                enable: false,
+                mode: "repulse",
+              },
+              onclick: {
+                enable: false,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
+    </React.Fragment>
+  );
+};
 
 export default Particle;
